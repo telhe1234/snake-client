1e0029fd0a76a34154ffb7bbc134ffb1ba286466
Taha Helwani <telhe008@uottawa.ca>
2021-11-03 22:33:42 +0000

    Added code to control the snake in all four directions

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+// Stores the active TCP connection object.
+let connection;
 
 const handleUserInput = function (key) {
   // your code here
@@ -6,10 +8,27 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+    // \u0077 maps to w input
+  if (key === '\u0077') {
+    connection.write('Move: up');
+  }
+  // \u0061 maps to a input
+  if (key === '\u0061') {
+    connection.write('Move: left');
+  }
+  // \u0077 maps to s input
+  if (key === '\u0073') {
+    connection.write('Move: down');
+  }
+  // \u0077 maps to d input
+  if (key === '\u0064') {
+    connection.write('Move: right');
+  }
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
