20ab3f8a3bc831e45c609dbc2802932e839af560
Taha Helwani <telhe008@uottawa.ca>
2021-11-03 21:21:22 +0000

    Refactored code to have all setupInput code in a separate input.js file

---
+++ input.js
@@ -0,0 +1,21 @@
+
+const handleUserInput = function (key) {
+  // your code here
+  // process.stdout.write('.');
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,26 +1,8 @@
-const {connect} = require('./client');
-
-const handleUserInput = function (key) {
-  // your code here
-  // process.stdout.write('.');
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
 setupInput();
