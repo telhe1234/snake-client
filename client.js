4019f8bc57e4ff80f8ed93f7acd760b11132708e
Taha Helwani <telhe008@uottawa.ca>
2021-11-03 20:00:25 +0000

    Added a connect event handler to the connect function and sent my name to the server

--- client.js
+++ client.js
@@ -12,6 +12,10 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log(data);
   });
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+    conn.write("Name: TE");
+  });
 
   return conn;
 };
